5635a4348f453362f86a215e07d7586befb91cfa
Carol Woo <cewoo98@hotmail.com>
2019-09-25 17:29:03 +0000

    more steps to our snake game

---
+++ client.js
@@ -0,0 +1,32 @@
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
+const connect = function () {
+  const conn = net.createConnection({
+    host: '172.46.2.204',
+    port: 50541
+
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', () => {
+    console.log(`Successfully connected to game server`);
+    conn.write('Name: SNE');
+  });
+
+
+
+  conn.on('data', (data) => {
+    console.log('you ded cuz you idled', data);
+  });
+
+
+
+  return conn;
+};
+
+module.exports = {
+  connect,
+};
